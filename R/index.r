11f2fb1e332a36435cc0da1ac85f0613e665461c
hadley <h.wickham@gmail.com>
2013-01-11 15:13:05 -0600

    Improve index documentation

--- R/index.r
+++ R/index.r
@@ -1,11 +1,16 @@
-#' Read/write the index.dcf file.
+#' Load/save the index.dcf file.
 #'
+#' An index object saves both the contents and the location of the file, so
+#' it can easily be resaved.
+#'
+#' @param path to file to load
+#' @param quiet if \code{TRUE} no output is produced.
 #' @examples
 #' index <- load_index(sample())
 #' index
 #' save_index(index)
-load_index <- function(path = "index.dcf", quiet = FALSE) {
-  message("Reading ", path)
+load_index <- function(path, quiet = FALSE) {
+  if (!quiet) message("Reading ", path)
 
   fields <- c("ID", "Authors", "Title", "Editor", "Reviewers", "Status")
   dcf <- read.dcf(path, fields = fields, keep.white = fields)
@@ -27,18 +32,13 @@ load_index <- function(path = "index.dcf", quiet = FALSE) {
   index(articles, path)
 }
 
-as.index <- function(index) {
-  if (is.index(index)) return(index)
-
-  path <- find_index(index)
-  load_index(path)
-}
-
 #' Find the index file.
 #'
 #' Looks first in \code{path} and then in the \code{index.dcf}
 #' option.
 #'
+#' @return For \code{find_index} a path, for \code{as.index} the
+#'   parsed index file.
 #' @export
 find_index <- function(path = NULL) {
   if (!is.null(path)) {
@@ -52,28 +52,48 @@ find_index <- function(path = NULL) {
   stop("Can't find index.dcf", call. = FALSE)
 }
 
+#' @rdname find_index
+#' @export
+as.index <- function(index) {
+  if (is.index(index)) return(index)
+
+  path <- find_index(index)
+  load_index(path)
+}
+
+
 #' @rdname load_index
-save_index <- function(index) {
+#' @param index index file to re-save.
+save_index <- function(index, quiet = FALSE) {
   stopifnot(is.index(index))
   stopifnot(!is.null(index$path))
 
   path <- index$path
-  message("Writing ", path)
+  if (!quiet) message("Writing ", path)
   cat(format(index), "\n", file = path)
 }
 
+#' The index class.
+#'
+#' @keywords internal
 index <- function(articles, path = NULL) {
   structure(list(articles = articles, path = path), class = "index")
 }
 
+#' @rdname index
+is.index <- function(x) inherits(x, "index")
+
+#' @S3method [ index
 "[.index" <- function(x, i) {
   index(x$articles[i])
 }
 
+#' @S3method [[ index
 "[[.index" <- function(x, i) {
   x$articles[[i]]
 }
 
+#' @S3method c index
 c.index <- function(..., recursive = FALSE) {
   pieces <- list(...)
 
@@ -92,37 +112,15 @@ c.index <- function(..., recursive = FALSE) {
   index
 }
 
-is.index <- function(x) inherits(x, "index")
-
+#' @S3method length index
 length.index <- function(x) length(x$articles)
 
+#' @S3method format index
 format.index <- function(x, ...) {
   articles <- lapply(x$articles, format)
   paste(articles, collapse = "\n\n")
 }
-print.index <- function(x, ...) cat(format(x), "\n")
 
+#' @S3method print index
+print.index <- function(x, ...) cat(format(x, ...), "\n")
 
-find_article <- function(id, index = NULL) {
-  stopifnot(is.character(id), length(id) == 1)
-  index <- as.index(index)
-
-  id <- parse_id(id)
-  article <- Filter(function(x) identical(x$id, id), index$articles)
-  if (length(article) == 0) {
-    stop("Can't find ", format(id), call. = FALSE)
-  }
-  if (length(article) > 1) {
-    stop("Found multiple articles with  ", format(id), call. = FALSE)
-  }
-  article[[1]]
-}
-
-update_article <- function(article, index = NULL) {
-  stopifnot(is.article(article))
-  index <- as.index(index)
-
-  i <- Position(function(x) identical(x$id, article$id), index$articles)
-  index$articles[[i]] <- article
-  index
-}
