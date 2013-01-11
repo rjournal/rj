cf580e05f9359ea483a58e862fa85aafd2ddda1d
hadley <h.wickham@gmail.com>
2013-01-11 15:23:30 -0600

    Test update

--- inst/tests/test-index.r
+++ inst/tests/test-index.r
@@ -1,7 +1,7 @@
 context("Index")
 
 test_that("Unparseable blocks written out as is", {
-  ind <- parse_index("index-parse-fail.dcf", quiet = TRUE)
+  ind <- load_index("index-parse-fail.dcf", quiet = TRUE)
   raw <- paste(readLines("index-parse-fail.dcf"), collapse = "\n")
 
   expect_equal(raw, format(ind))
