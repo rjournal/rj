1b753feb367c290ff64931362c2d7fa0dfb5b64f
hadley <h.wickham@gmail.com>
2013-04-12 13:13:39 -0500

    Auto reviewer invitations

--- R/templates.r
+++ R/templates.r
@@ -24,3 +24,30 @@ render_template <- function(article, template) {
 
   whisker.render(readLines(template), as.data(article))
 }
+
+invite_reviewers <- function(article, prefix = "1") {
+  article <- as.article(article)
+  for(i in seq_along(article$reviewers)) {
+    invite_reviewer(article, i, prefix = prefix)
+  }
+}
+
+invite_reviewer <- function(article, reviewer_id, prefix = "1") {
+  article <- as.article(article)
+  reviewer <- article$reviewers[[reviewer_id]]
+
+  data <- as.data(article)
+  data$email <- reviewer$email
+  data$name <- reviewer$name
+  data$date <- format(Sys.Date() + 30, "%d %b %Y")
+
+  dest <- file.path(article$path, "correspondence")
+  if (!file.exists(dest)) dir.create(dest)
+  name <- paste0(prefix, "-invite-", reviewer_id, ".txt")
+
+  template <- find_template("review")
+  email <- whisker.render(readLines(template), data)
+  writeLines(email, file.path(dest, name))
+
+  email_text(email)
+}
