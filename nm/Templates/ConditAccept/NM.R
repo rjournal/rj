40d6d3d9026bdc951503ab2d013525e6bbb1b315
Norm Matloff <matloff@cs.ucdavis.edu>
2019-03-02 15:02:18 -0800

    Dr. X

--- nm/Templates/ConditAccept/NM.R
+++ nm/Templates/ConditAccept/NM.R
@@ -1,6 +1,8 @@
 
+# most will be Dr. Someone, and the rest won't mind
+
 formletter <- c(
-"\n\nHello, GREET.  Norm Matloff of the R Journal editorial board here.",
+"\n\nHello, Dr. GREET.  Norm Matloff of the R Journal editorial board here.",
 "I am happy to inform you that I am accepting your paper, TITLE, for publication, subject to your addressing the reviewer comments, attached.", 
 "Please send me your revised files -- ONLY the ones that you change, please -- as well as an itemized document -- plain text or PDF, please -- explaining the changes you made, and the reasons for declining to make some of the suggested changes.",
 "In your revised files, please make sure to continue to comply with R Journals formatting rules etc."
