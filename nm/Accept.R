1ea3230373d032f13a48cde4ae6dcaa22aa4af45
Norm Matloff <matloff@cs.ucdavis.edu>
2019-04-13 16:25:37 -0700

    accept() done

--- nm/Accept.R
+++ nm/Accept.R
@@ -1,26 +1,30 @@
 
 ######################  accept  ##################################
 
-# manuscript number msNum: edit DESCRIPTION
+# accepts manuscript number msNum: edit DESCRIPTION; push DESCRIPTION
+# and new author files
 
 # run from Submissions/
 
-accept <- function(msNum) {
+accept <- function(msNum,newAutFiles='') {
    # updated author files pushed to GitHub yet?
-   readline('push new author files, if any, to GitHub, then hit Enter): ')
-   # get in-memory DESCRIPTION file
-   if (is.null(desFiles))  {
-      getAll()
-      print('desFiles regenerated')
-   }
-   des <- desFiles[[msNum]]
+   print('the new author files to be pushed to GitHub are:')
+   cat(newAutFiles,'\n')
+   ans <- readline('correct files (yes or no)? ')
+   if (ans != 'yes') stop('wrong file list')
+   if (newAutFiles != '') 
+      pushToGitHub(newAutFiles,'new author files')
+   # get in-memory DESCRIPTION file for this ms
+   getAll()
+   des <- desFiles[[msNum]]  
    # double check it's the right one
    cat('\ncheck correct ms!\n')
    print(des)
-   ans <- readline('correct file (yes or no) ')
+   ans <- readline('correct file (yes or no)? ')
    if (ans != 'yes') stop('wrong file')
    # add 'accept' line to DESCRIPTION file
    accLine <- paste0('  ',Sys.Date(),' accept')
+   # allow for Roger Bivand-style comment
    cmt <- readline('optional brief comment (if none, hit Enter): ')
    if (length(cmt) > 0)
       accLine <- paste0(accLine,' [',cmt,']')
@@ -30,13 +34,10 @@ accept <- function(msNum) {
    outfilename <- paste0(msNum,'/DESCRIPTION')
    cat(des,file=outfilename,sep='\n')
    # push to GitHub
-   cmd <- makeSysCmd('git add ',outfilename)
-   cmd()
+   pushToGitHub('DESCRIPTION','"accept"')
    cmd <- makeSysCmd('git commit -m "condit. accept"')
    cmd()
    # commit may take a while
-   readline('hit Enter when ready')
-   ghPush()
    autinfo <- getAutInfo(des)
    sendLetter(msNum,autinfo[1],autinfo[2],'"your R Journal submission"',
       'AcceptTmplt.R')
