8ba2b541b9558532c2b2721decb5def08b7de9d3
Norm Matloff <matloff@cs.ucdavis.edu>
2019-06-05 21:37:17 -0700

    typo

--- nm/AcceptTmplt.R
+++ nm/AcceptTmplt.R
@@ -3,7 +3,7 @@
 
 formletter <<- 
 c(
-'\n\nHello, Dr. GREET.  EDITOR of the R Journal editorial board here.  I am happy to inform you that I am accepting your paper, TITLE, for publication.",
+'\n\nHello, Dr. GREET.  EDITOR of the R Journal editorial board here.  I am happy to inform you that I am accepting your paper, TITLE, for publication.',
 paste0('\n',Sys.getenv('RJ_NAME'))
 )
 
