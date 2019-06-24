ac9d11d18da0bd9079772dfb5e86b26964752a17
Norm Matloff <matloff@cs.ucdavis.edu>
2019-06-23 20:42:42 -0700

    allows multiple ms's

--- nm/ACK.R
+++ nm/ACK.R
@@ -2,23 +2,31 @@
 
 ###############  acknowledge receipt of submissions #####################
 
-ack <- function(msNum) {
+# msNums: char vec of ms numbers to be ACKed
+
+ack <- function(msNums) {
    # get in-memory DESCRIPTION file
    if (!exists('desFiles'))  {
       getAll()
       print('desFiles regenerated')
    }
-   des <- desFiles[[msNum]]
-   cat('\ncheck correct ms!\n')
-   print(des)
-   ans <- readline('correct file (yes or no) ')
-   if (ans != 'yes') stop('wrong file')
+   # get formletter (global; no need, should change this)
    source('../../rj/nm/ACKTmplt.R')
-   autinfo <- getAutInfo(des)
-   formletter[1] <- sub('GREET',autinfo[1],formletter[1])
-   title <- des[1]
-   title <- substr(title,8,nchar(title))
-   formletter[1] <- sub('TITLE',title,formletter[1])
-   mailIt(autinfo[2],'"R Journal manuscript received"',NULL,formletter)
+   for (msNum in msNums) {
+      cat('ACKing',msNum,'\n')
+      des <- desFiles[[msNum]]
+      cat('\ncheck correct ms!\n')
+      print(des)
+      ans <- readline('correct file (yes or no) ')
+      if (ans != 'yes') stop('wrong file')
+      autinfo <- getAutInfo(des)
+      frml <- formletter
+      frml[1] <- sub('GREET',autinfo[1],frml[1])
+      title <- des[1]
+      title <- substr(title,8,nchar(title))
+      frml[1] <- sub('TITLE',title,frml[1])
+      print(frml)
+      mailIt(autinfo[2],'"R Journal manuscript received"',NULL,frml)
+   }
 }
 
