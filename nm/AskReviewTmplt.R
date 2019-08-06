da286f9dad2f830ecde38e90e7e13e3c78f218f1
Norm Matloff <matloff@cs.ucdavis.edu>
2019-08-06 14:14:59 -0700

    less wordy

--- nm/AskReviewTmplt.R
+++ nm/AskReviewTmplt.R
@@ -1,6 +1,6 @@
 
 formletter <<- c(
-   "\n\nHello, GREET.  EDITOR of the R Journal editorial board here.  I am writing to you in my capacity as an editor on the R Journal, and I have a paper that I hope you might have time to review, TITLE.", 
+   "\n\nHello, GREET.  EDITOR here.  I am writing to you in my capacity as an editor on the R Journal, and I have a paper that I hope you might have time to review, TITLE.", 
    "The R Journal is not mathematical. The major points a referee should address are questions such as: Does the software look useful to a reasonably broad audience, and have the authors made a good case for this?  Are there enhancements or changes that the authors should consider?  Is the presentation clear, and (this is very important) have the authors sufficiently explained the methodology used?  Are the references complete? Etc.  The referee of course need not verify the correctness of the code.",
    "The package is on CRAN.",
    "The paper and any supplementary code are attached.",
