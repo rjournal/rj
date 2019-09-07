c156e6fc1a8143f04578697a5e177d6374ab2a96
Norm Matloff <matloff@cs.ucdavis.edu>
2019-09-07 14:34:02 -0700

    folding nm/ into rj/nm

--- R/ConditAccTmplt.R
+++ R/ConditAccTmplt.R
@@ -8,7 +8,7 @@ I am happy to inform you that I am accepting your paper, TITLE, for publication,
 "Please send me your revised files -- ONLY the ones that you change, please -- as well as an itemized document -- plain text or PDF, please -- explaining the changes you made, and the reasons for declining to make some of the suggested changes.",
 "Please do NOT highlight your changes.",
 "In your revised files, please make sure to continue to comply with R Journals formatting rules etc.",
-"In particular: Only 1 .bib file; only 1 .tex file other than RJwrapper.tex; no \cite's of the R language, and no such listing in .bib.",
+"In particular: Only 1 .bib file; only 1 .tex file other than RJwrapper.tex; no cite's of the R language, and no such listing in .bib.",
 paste0('\n',Sys.getenv('RJ_NAME'))
 )
 
