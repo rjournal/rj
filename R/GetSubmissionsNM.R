3c9736fd289747708457316d120e6841f70332d7
Norm Matloff <matloff@cs.ucdavis.edu>
2019-09-11 19:08:11 -0700

    this file not ready yet

---
+++ R/GetSubmissionsNM.R
@@ -0,0 +1,26 @@
+
+# wraps to rj::get_submissions(), then does a different ACK, with the
+# latter calling rj::build_latex() as well
+
+# call from articles/
+
+# work in progress
+
+# newSubmissions <- function() {
+#    require(gitR)
+#    browser()
+#    oldSubs <- dir('Submissions')
+#    # can't have any files other that article directories
+#    ds <- list.dirs('Submissions',recursive=FALSE)
+#    if (length(oldSubs) > length(ds))
+#       stop('remove non-article files/directories first')
+#    # rj:::get_submissions()
+#    currentSubs <- dir('Submissions')
+#    newSubs <- setdiff(currentSubs,oldSubs)
+#    if (currentSubs == newSubs) load('newSubs')
+#    cat('new submissions:\n',newSubs)
+#    save(newSubs,file='newSubs')
+#    ACK(newSubs)
+#    gitOpPush(newSubs,'"new submissions"')
+# }
+
