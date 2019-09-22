3b4919e93fb57c8fbe8ca713bc1c5eff7437c6b7
Norman Matloff <matloff@cs.ucdavis.edu>
2019-09-22 16:10:52 -0700

    progress

--- R/CheckNewSubmitNM.R
+++ R/CheckNewSubmitNM.R
@@ -1,20 +1,28 @@
 
 # various checks for readiness to review
 
-# run from articles/
+# run from article's directory
 
-checkNewSubmit <- function(msnum) {
-   setwd(paste0('Submissions/',msnum))
-   # finicky rj software can only allow for 2 .tex and 1 .bib
+checkNewSubmit <- function() 
+{
+   
+   # rj software can only allow for 2 .tex and 1 .bib
    if (length(dir(pattern=glob2rx('*.tex*'))) > 2)
-      stop('only 2 *.tex* allowed')
+      warnReArticle('only 2 *.tex* allowed')
    if (length(dir(pattern=glob2rx('*.bib*'))) > 1)
-      stop('only 1 *.bib* allowed')
+      warnReArticle('only 1 *.bib* allowed')
    # and only 1 ref to .tex in RJwrapper.tex, even in comments
    texs <- grep('.tex',readLines('RJwrapper.tex'))
    if (length(texs) > 1)
-      stop('only 1 mention allowed in RJwrapper.tex of ".tex"')
+      warnReArticle('only 1 mention allowed in RJwrapper.tex of ".tex"')
    # run LaTeX checks
    rj::build_latex('RJwrapper.tex')
 }
 
+warnReArticle <- function(msg) 
+{
+   tmp <- getwd()
+   tmp <- strsplit(tmp,'/')[[1]]
+   msNum <- tmp[length(tmp)]
+   warning(paste0(msNum,': ',msg))
+}
