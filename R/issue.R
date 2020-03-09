64bd89a768b1be13fdfb6b3c5c960d4e126dbc31
Mitchell <mail@mitchelloharawild.com>
2020-03-09 12:44:10 +1100

    Better suffix replacement: allow arbitrary many refs and many empty []

--- R/issue.R
+++ R/issue.R
@@ -585,10 +585,20 @@ get_post_md <- function(dir="news", file=c("foundation"=1, "cran"=1, "bioc"=1, "
 #' @export
 suffix_labels <- function(file, suffix, macros = c("label", "ref", "pageref", "bibitem", "cite", "citep", "citealt", "citet", "citealp")){
   macros <- paste0(macros, collapse = "|")
-  pattern <- paste0("\\\\(", macros, ")\\s*(\\[([^\\]]+)\\])?\\s*\\{([^\\}]+)\\}")
-  replacement <- paste0("\\\\\\1\\2{\\4-", suffix, "}")
+  pattern <- paste0("\\\\(", macros, ")\\s*((\\[([^\\]]*)\\]\\s*)*)\\{([^\\}]+)\\}")
   txt <- readLines(file)
-  txt <- stringr::str_replace_all(paste(txt, collapse = "\n"), pattern, replacement)
+  txt <- stringr::str_replace_all(
+    paste(txt, collapse = "\n"), pattern, 
+    function(x){
+      x <- stringr::str_match(x, pattern)
+      sprintf(
+        "\\%s%s{%s}", 
+        x[,2], if(is.na(x[,3])) "" else x[,3],
+        paste(stringr::str_trim(stringr::str_split(x[,6], ",")[[1]]), suffix, 
+              sep = "-", collapse = ", ")
+      )
+    }
+  )
   writeLines(txt, file)
 }
 
