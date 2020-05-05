07913e4ad50748120c032feb139c2a594a86491e
dicook <visnut@gmail.com>
2020-05-05 16:42:43 +1000

    removed a browser function

--- R/reviewers.R
+++ R/reviewers.R
@@ -61,7 +61,6 @@ invite_reviewer <- function(article, reviewer_id, prefix = "1") {
     data$email <- reviewer$email
     data$name <- reviewer$name
     data$firstname <- stringr::str_split(reviewer$name, " ")[[1]][1]
-    browser()
     data$date <- format(Sys.Date() + 30, "%d %b %Y")
     
     template <- find_template("review")
