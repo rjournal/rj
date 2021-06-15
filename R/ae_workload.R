d0ee3eab0535b718acb7dca61f6c25230a7f7723
Sherry <huizezhangsh@gmail.com>
2021-06-15 23:01:51 +1000

    update ae_workload using github API

--- R/ae_workload.R
+++ R/ae_workload.R
@@ -1,9 +1,26 @@
 #' Check the number of articles an AE is currently working on
+#'
+#' The function Github REST API to query the ae prefixed repository and extract the content in the submissions folder.
+#'
+#' @importFrom gh gh
+#' @importFrom stringr str_detect
+#' @importFrom dplyr mutate count
+#' @importFrom tidyr unnest
 #' @export
 ae_workload <- function(){
-  submitted_articles <- dir(file.path(get_articles_path(), "Submissions"))
 
-  arts <- map(submitted_articles, as.article)
+  org_repos <- gh("GET /orgs/{org}/repos", org = "rjournal", type = "all")
 
-  length(arts)
+  repo <- vapply(org_repos, "[[", "", "name")
+
+  ae_repo <- repo[stringr::str_detect(repo, "ae")]
+
+  tibble::tibble(ae = ae_repo) %>%
+    dplyr::mutate(folder = list(gh("GET /repos/{owner}/{repo}/contents/{path}",
+                                   owner = "rjournal",
+                                   repo = ae_repo,
+                                   path = "Submissions"))) %>%
+    tidyr::unnest(folder) %>%
+    dplyr::count(ae)
+
 }
